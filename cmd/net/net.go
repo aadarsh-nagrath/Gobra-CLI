08d765a4fd6808ab6bb23a620fe0947c818f2799
Aadarsh Nagrath <anagrath1@gmail.com>
2023-10-30 16:08:25 -0400

    Building cli

---
+++ cmd/net/net.go
@@ -0,0 +1,35 @@
+/*
+Copyright © 2023 NAME HERE anagrath1@gmail.com
+
+*/
+package net
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// netCmd represents the net command
+var netCmd = &cobra.Command{
+	Use:   "net",
+	Short: "net is a pkg to hold network related commands",
+	Long: ``,
+	Run: func(cmd *cobra.Command, args []string) {
+		cmd.Help()
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(netCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// netCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// netCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
